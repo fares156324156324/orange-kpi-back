bbc6ef4efbcdd12a5b0ebdd113bdc225a299c349
Fares Louati <medfares.louati@esprit.tn>
2023-03-15 00:34:28 +0100

    first commit

---
+++ src/main/java/com/example/backend_jenkins_app/demo/JenkinsTestController.java
@@ -0,0 +1,15 @@
+package com.example.backend_jenkins_app.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+
+public class JenkinsTestController {
+    @RequestMapping("/")
+ public String index()
+ {
+    return "Testing spring boot jenkins pipeline";
+ }
+
+    
+}
