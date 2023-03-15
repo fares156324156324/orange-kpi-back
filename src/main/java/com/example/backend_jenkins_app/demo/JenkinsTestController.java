b16fd3a44223bc2aaab0a47a7aad5aef20e2373a
root <medfares.louati@esprit.tn>
2023-03-14 20:28:06 -0700

    last commit

--- src/main/java/com/example/backend_jenkins_app/demo/JenkinsTestController.java
+++ src/main/java/com/example/backend_jenkins_app/demo/JenkinsTestController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 
 public class JenkinsTestController {
-    @RequestMapping("/hello")
+    @RequestMapping("/")
  public String index()
  {
     return "Testing spring boot testttttttt jenkins pipeline";
