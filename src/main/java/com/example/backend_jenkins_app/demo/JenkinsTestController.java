463418fdc7149e77dea530da3e8515d6463dbe37
root <medfares.louati@esprit.tn>
2023-03-17 22:26:15 -0700

    testt

--- src/main/java/com/example/backend_jenkins_app/demo/JenkinsTestController.java
+++ src/main/java/com/example/backend_jenkins_app/demo/JenkinsTestController.java
@@ -8,7 +8,7 @@ public class JenkinsTestController {
     @RequestMapping("/")
  public String index()
  {
-    return "Testing spring boot tettt jenkins pipeline";
+    return "Testing soot tettt jenkins pipeline";
  }
 
     
