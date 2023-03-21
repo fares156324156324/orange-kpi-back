e177fb8e6f7c3a6a516378e833ec8149b2b6b63e
root <medfares.louati@esprit.tn>
2023-03-20 17:57:59 -0700

    seqdq

--- src/main/java/com/example/backend_jenkins_app/demo/JenkinsTestController.java
+++ src/main/java/com/example/backend_jenkins_app/demo/JenkinsTestController.java
@@ -8,7 +8,7 @@ public class JenkinsTestController {
     @RequestMapping("/")
  public String index()
  {
-    return "Testin jenkins pipeline";
+    return "This is a test of the SpringBoot app";
  }
 
     
