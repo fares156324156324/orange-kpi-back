a29892c2383906f91d735bb5102c24849c3987dc
root <medfares.louati@esprit.tn>
2023-05-08 09:45:31 -0700

    test pip

--- src/main/java/com/example/backend_jenkins_app/services/IUserService.java
+++ src/main/java/com/example/backend_jenkins_app/services/IUserService.java
@@ -5,13 +5,13 @@ import java.util.List;
 import com.example.backend_jenkins_app.models.User;
 
 public interface IUserService {
-    
+
     public User AddUser(User user);
-    
-    public List<User> getAllUsers(); 
-    
+
+    public List<User> getAllUsers();
+
     public User getUserById(int id);
-    
-	public void DeleteUser(int id);
-		
-	}
+
+    public void DeleteUser(int id);
+
+}
