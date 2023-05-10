32e9b99680942c8cc5aca583482e1ae0a53d765d
root <medfares.louati@esprit.tn>
2023-05-10 09:52:54 -0700

    pipeli

--- src/main/java/com/example/backend_jenkins_app/demo/JenkinsTestController.java
+++ src/main/java/com/example/backend_jenkins_app/demo/JenkinsTestController.java
@@ -2,14 +2,13 @@ package com.example.backend_jenkins_app.demo;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 
 public class JenkinsTestController {
-    @RequestMapping("/")
- public String index()
- {
-    return "dqsdsqdqsd appp";
- }
+   @RequestMapping("/")
+   public String index() {
+      return "dqsqsd appp";
+   }
 
-    
 }
