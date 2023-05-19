efa8244283aa81083eeac742744660edee4b0092
root <medfares.louati@esprit.tn>
2023-05-19 13:14:41 -0700

    test

--- src/main/java/com/example/backend_jenkins_app/demo/JenkinsTestController.java
+++ src/main/java/com/example/backend_jenkins_app/demo/JenkinsTestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class JenkinsTestController {
    @RequestMapping("/test")
    public String index() {
-      return " Tsti pt ";
+      return " Testtttttttttttttttttt       pt ";
    }
 
 }
