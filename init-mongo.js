7dba772d383c0b81d4d04cd816961b14483bbd3b
root <medfares.louati@esprit.tn>
2023-05-25 16:05:39 -0700

    database

--- init-mongo.js
+++ init-mongo.js
@@ -1,13 +1,6 @@
-db.createUser(
-    {
-        user: "fares",
-        pwd: "fares",
-        roles: [
-            {
-                role: "readWrite",
-                db: "fares"
-            }
-        ]
-    }
-);
-db.createCollection("Projectdb"); //MongoDB creates the database when you first store data in that database
+db = db.getSiblingDB('Projectdb');
+db.createUser({
+  user: 'fares',
+  pwd: 'password',
+  roles: [{ role: 'readWrite', db: 'Projectdb' }]
+});
