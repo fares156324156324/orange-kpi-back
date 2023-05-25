1e1bfc63f2804584a986413d43d5ad5636020ed4
root <medfares.louati@esprit.tn>
2023-05-25 16:12:16 -0700

    database

--- src/main/java/com/example/backend_jenkins_app/repositories/GroupRepository.java
+++ src/main/java/com/example/backend_jenkins_app/repositories/GroupRepository.java
@@ -1,8 +1,11 @@
 package com.example.backend_jenkins_app.repositories;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.stereotype.Repository;
 import com.example.backend_jenkins_app.models.Group;
+
+@EnableMongoRepositories(basePackageClasses = GroupRepository.class)
 
 @Repository
 
--- src/main/java/com/example/backend_jenkins_app/repositories/UserRepository.java
+++ src/main/java/com/example/backend_jenkins_app/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.example.backend_jenkins_app.repositories;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import com.example.backend_jenkins_app.models.User;
 import org.springframework.stereotype.Repository;
 
+@EnableMongoRepositories(basePackageClasses = UserRepository.class)
 @Repository
 public interface UserRepository extends MongoRepository<User, Integer> {
 
