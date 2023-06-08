932ba0103ecc2e01dcdf7c89f1d5af84eccaec99
fares <admin@example.com>
2023-06-08 07:40:05 -0700

    Add user

--- src/main/java/com/example/backend_jenkins_app/models/User.java
+++ src/main/java/com/example/backend_jenkins_app/models/User.java
@@ -2,36 +2,29 @@ package com.example.backend_jenkins_app.models;
 
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import lombok.ToString;
 
 @Data
-@Getter
-@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
 @Document(collection = "users")
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private int userId;
+    private String id;
 
     private String userName;
     private String email;
     private String password;
-
-    @Enumerated(EnumType.STRING)
+    
     private String role;
 
     @DBRef
