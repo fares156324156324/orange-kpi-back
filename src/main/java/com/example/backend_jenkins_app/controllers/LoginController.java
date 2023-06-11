2bd6b700d5c39c5c9622832826b9a3e9b13d251f
fares <admin@example.com>
2023-06-11 11:29:16 -0700

    Authentification JWT included

---
+++ src/main/java/com/example/backend_jenkins_app/controllers/LoginController.java
@@ -0,0 +1,55 @@
+package com.example.backend_jenkins_app.controllers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import com.example.backend_jenkins_app.Security.JWTUtil;
+import com.example.backend_jenkins_app.models.User;
+import com.example.backend_jenkins_app.repositories.GroupRepository;
+import com.example.backend_jenkins_app.services.IGroupService;
+import com.example.backend_jenkins_app.services.IUserService;
+
+
+
+
+public class LoginController {
+    
+    @Autowired
+    private JWTUtil jwtUtil;
+
+    @Autowired
+    private GroupRepository groupRepository;
+    @Autowired
+
+    private IGroupService groupService;
+    @Autowired
+
+    private IUserService userService;
+
+    
+
+
+    
+    
+   @PostMapping("/login")
+public ResponseEntity<String> login(@RequestBody User user) {
+    String email = user.getEmail();
+    String password = user.getPassword();
+    
+    User authenticatedUser = userService.authenticateUser(email, password);
+    if (authenticatedUser != null) {
+        // User authentication successful
+        // Generate a JWT token
+        String token = jwtUtil.generateToken(email);
+        
+        // Return the JWT token in the response body
+        return ResponseEntity.ok(token);
+    } else {
+        // User authentication failed
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
+    }
+}
+
+
+}
