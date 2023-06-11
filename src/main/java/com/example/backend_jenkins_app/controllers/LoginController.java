ff96cde5a3df6c2c9f0a7202fc0a00457e1cdfa9
fares <admin@example.com>
2023-06-11 11:35:41 -0700

    Update User Group

--- src/main/java/com/example/backend_jenkins_app/controllers/LoginController.java
+++ src/main/java/com/example/backend_jenkins_app/controllers/LoginController.java
@@ -4,6 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import com.example.backend_jenkins_app.Security.JWTUtil;
 import com.example.backend_jenkins_app.models.User;
 import com.example.backend_jenkins_app.repositories.GroupRepository;
@@ -12,6 +14,7 @@ import com.example.backend_jenkins_app.services.IUserService;
 
 
 
+@RequestMapping("/login")
 
 public class LoginController {
     
@@ -32,7 +35,6 @@ public class LoginController {
 
     
     
-   @PostMapping("/login")
 public ResponseEntity<String> login(@RequestBody User user) {
     String email = user.getEmail();
     String password = user.getPassword();
