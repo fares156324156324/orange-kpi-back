db7ec364fcf8a27697abcf5fe1de0d0ba4675860
fares <admin@example.com>
2023-06-11 12:12:07 -0700

    Update User Group

--- src/main/java/com/example/backend_jenkins_app/controllers/LoginController.java
+++ src/main/java/com/example/backend_jenkins_app/controllers/LoginController.java
@@ -6,19 +6,23 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.example.backend_jenkins_app.Security.JWTUtil;
 import com.example.backend_jenkins_app.models.User;
 import com.example.backend_jenkins_app.repositories.GroupRepository;
 import com.example.backend_jenkins_app.services.IGroupService;
 import com.example.backend_jenkins_app.services.IUserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 
 
 @RestController
 @RequestMapping("/login")
 
 public class LoginController {
-    
+    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+
     @Autowired
     private JWTUtil jwtUtil;
 
@@ -31,23 +35,29 @@ public class LoginController {
 
     private IUserService userService;
 
+    
+
     @PostMapping
+
     public ResponseEntity<String> login(@RequestBody User user) {
     String email = user.getEmail();
     String password = user.getPassword();
     
     User authenticatedUser = userService.authenticateUser(email, password);
+    logger.info("Authenticated User: {}", authenticatedUser); // Log the authenticatedUser
+
     if (authenticatedUser != null) {
         // User authentication successful
         // Generate a JWT token
-        //String token = jwtUtil.generateToken(email);
+        String token = jwtUtil.generateToken(email);
         
         // Return the JWT token in the response body
-        return ResponseEntity.ok("");
+        return ResponseEntity.ok(token);
     } else {
         // User authentication failed
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials" + email + ' ' + password);
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
     }
+    
 }
 
 
