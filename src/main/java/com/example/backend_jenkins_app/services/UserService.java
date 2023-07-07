f53c7056936ad83809c432ce3a13a931c76dc2ae
fares <admin@example.com>
2023-07-06 18:11:52 -0700

    Auth jwt restest

--- src/main/java/com/example/backend_jenkins_app/services/UserService.java
+++ src/main/java/com/example/backend_jenkins_app/services/UserService.java
@@ -1,9 +1,13 @@
 package com.example.backend_jenkins_app.services;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -16,7 +20,7 @@ import io.jsonwebtoken.SignatureAlgorithm;
 
 @Service
 
-public class UserService implements IUserService {
+public class UserService implements IUserService, UserDetailsService {
     @Autowired
     private UserRepository userRepository;
 
@@ -34,21 +38,19 @@ public class UserService implements IUserService {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-
-    public String generateJwtToken(User user) {
-        Date now = new Date();
-        Date expiration = new Date(now.getTime() + jwtExpiration);
-
-        return Jwts.builder()
-                .setSubject(user.getEmail())
-                .setIssuedAt(now)
-                .setExpiration(expiration)
-                .signWith(SignatureAlgorithm.HS512, jwtSecret)
-                .compact();
-    }
     @Override
     public User addUser(User user) {
         return userRepository.save(user);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found with email: " + email);
+        }
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(),
+                new ArrayList<>());
     }
 
     @Override
@@ -77,8 +79,19 @@ public class UserService implements IUserService {
     }
     @Override
     public User authenticateUser(String email, String password) {
-        return userRepository.findByEmailAndPassword(email, password);
+        UserDetails userDetails = loadUserByUsername(email);
+        if (passwordEncoder.matches(password, userDetails.getPassword())) {
+            return getUserByEmail(email);
+        }
+        return null;
     }
+
+    @Override
+    public String generateJwtToken(User user) {
+        UserDetails userDetails = loadUserByUsername(user.getEmail());
+        return jwtUtil.generateToken(userDetails);
+    }
+    
 
 }
 
