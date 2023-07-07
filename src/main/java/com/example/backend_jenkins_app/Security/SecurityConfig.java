f52700c8af1f6e414f15e4adcc96fc25f6dd79a7
fares <admin@example.com>
2023-07-06 18:55:47 -0700

    Auth jwt restest v2

--- src/main/java/com/example/backend_jenkins_app/Security/SecurityConfig.java
+++ src/main/java/com/example/backend_jenkins_app/Security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.backend_jenkins_app.Security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,44 +12,58 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.example.backend_jenkins_app.services.UserService;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-     
+
+    private final UserService userService;
+    private final JwtUtil jwtUtil;
+
+    public SecurityConfig(UserService userService, JwtUtil jwtUtil) {
+        this.userService = userService;
+        this.jwtUtil = jwtUtil;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-            .csrf().disable()
-            .authorizeRequests()
+        http.csrf().disable()
+                .authorizeRequests()
                 .antMatchers("/users/login").permitAll()
                 .antMatchers("/users/getAll").authenticated()
                 .anyRequest().authenticated()
                 .and()
-            .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED) // Set session creation policy to IF_REQUIRED
-                .maximumSessions(1) // Allow only one session per user
-                    .maxSessionsPreventsLogin(true) // Prevent concurrent sessions
-                    .expiredUrl("/login") // Redirect URL when session expires
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                .maximumSessions(1)
+                .maxSessionsPreventsLogin(true)
+                .expiredUrl("/login")
                 .and()
-            .and()
-            .httpBasic();
+                .and()
+                .httpBasic();
+
+        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
-    
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-            .withUser("admin").password(passwordEncoder().encode("admin123")).roles("USER");
+        auth.userDetailsService(userService).passwordEncoder(passwordEncoder());
     }
-    
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-    
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() throws Exception {
+        return new JwtAuthenticationFilter(jwtUtil, userService, authenticationManagerBean());
+    }
+
     @Override
     @Bean
     public AuthenticationManager authenticationManagerBean() throws Exception {
--- src/main/java/com/example/backend_jenkins_app/services/UserService.java
+++ src/main/java/com/example/backend_jenkins_app/services/UserService.java
@@ -1,9 +1,13 @@
 package com.example.backend_jenkins_app.services;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import com.example.backend_jenkins_app.models.User;
@@ -14,7 +18,7 @@ import io.jsonwebtoken.SignatureAlgorithm;
 
 @Service
 
-public class UserService implements IUserService {
+public class UserService implements IUserService ,UserDetailsService{
     @Autowired
     private UserRepository userRepository;
 
@@ -72,6 +76,15 @@ public class UserService implements IUserService {
     @Override
     public User authenticateUser(String email, String password) {
         return userRepository.findByEmailAndPassword(email, password);
+    }
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found with email: " + email);
+        }
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(),
+                new ArrayList<>());
     }
 
 }
