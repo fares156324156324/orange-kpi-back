cc8969e9d6dffb9b7d0de4bac887cb003dba4751
fares <admin@example.com>
2023-07-13 16:53:12 -0700

    backend update

---
+++ src/main/java/com/example/backend_jenkins_app/services/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package com.example.backend_jenkins_app.services;
+
+import com.example.backend_jenkins_app.models.User;
+import com.example.backend_jenkins_app.repositories.UserRepository;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(), user.getPassword(), new ArrayList<>());
+    }
+}
