9e1a27e3ca4b978d9be80a2a41342af052a82fd1
fares <admin@example.com>
2023-07-21 17:13:35 -0700

    backend last update

--- src/main/java/com/example/backend_jenkins_app/controllers/GroupController.java
+++ src/main/java/com/example/backend_jenkins_app/controllers/GroupController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,8 @@ import com.example.backend_jenkins_app.models.Group;
 import com.example.backend_jenkins_app.models.User;
 import com.example.backend_jenkins_app.services.IGroupService;
 
+
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/group")
 public class GroupController {
